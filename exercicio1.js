cfcf3fdb84985ac5d4ef14ee155b9916553d8888
Gabriel <gabriel_sep@outlook.com>
2023-02-09 22:34:01 -0300

    IF ELSE

--- exercicio1.js
+++ exercicio1.js
@@ -9,3 +9,17 @@ else{
     console.log("Entrou no Else")
 }
 console.log("Fim")*/
+
+var n1 = 10
+var n2 = 5
+var media = (n1+n2)/2
+console.log(media)
+if(media>=7){
+    console.log("Aprovado")
+}
+else if(media>=5){
+    console.log("Recuperação")
+}
+else{
+    console.log("Reprovado")
+}
